68526c34cc132eedddbfdbd37cfd9ce24d0a3353
Skrimned <adamfadhillah2006@gmail.com>
2026-04-29 22:26:18 +0700

    Fitur Metode Pembayaran

--- src/com/Kohisop/Qris.java
+++ src/com/Kohisop/Qris.java
@@ -1,14 +1,36 @@
 package com.Kohisop;
-// ini masih proof of concept, masih bisa salah dan perlu di modif
-public class Qris extends MetodeBayar{
+
+public class Qris extends MetodeBayar {
+
+    public Qris() {
+        this.name = "QRIS";
+        this.wallet = 0;
+    }
 
     @Override
     public void topUp(double amount) {
-        // definisiin
+        this.wallet += amount;
+        System.out.printf("Top up QRIS sebesar %.2f (IDR) berhasil. Saldo: %.2f (IDR)%n", amount, this.wallet);
     }
 
     @Override
     public void pay(double amount) {
-        //definisiin
+        this.wallet -= amount;
+        System.out.printf("Pembayaran QRIS sebesar %.2f (IDR) berhasil. Saldo: %.2f (IDR)%n", amount, this.wallet);
+    }
+
+    @Override
+    public int getDiskon() {
+        return 5; // diskon 5%
+    }
+
+    @Override
+    public int getBiayaAdmin() {
+        return 0; // tidak ada biaya admin
+    }
+
+    @Override
+    public String getNamaBayar() {
+        return "QRIS";
     }
 }
