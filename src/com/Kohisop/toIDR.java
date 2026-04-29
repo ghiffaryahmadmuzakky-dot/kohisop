ad41a4d98f1bb718e2f3103630de87acbc2fb45a
ARIEL FARDIAN AYSEL <arielfardianaysel@gmail.com>
2026-04-29 14:41:22 +0700

    feat: penambahan fitur mata uang

---
+++ src/com/Kohisop/toIDR.java
@@ -0,0 +1,9 @@
+package com.Kohisop;
+
+public class toIDR extends TukarUang {
+    @Override
+    public double Tukar(double Total){return Total;}
+
+    @Override
+    public String getMataUang() {return "IDR";}
+}
